0ea5d2635ca7c4563b053d973bff464cb9e45c19
Alexey Bystrov <alexeybystrov@me.com>
2020-09-10 19:37:18 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+// import 'bootstrap';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+// import 'bootstrap/dist/css/bootstrap-grid.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 
-const component = () => {
+/* const component = () => {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -9,3 +16,4 @@ const component = () => {
 };
 
 document.body.appendChild(component());
+ */
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
     path: `${__dirname}/dist`,
     filename: 'index_bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Custom template',
