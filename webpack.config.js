ff908a4fcc21ef86227eaf22cf4f2282d81d6feb
Alexey Bystrov <alexeybystrov@me.com>
2020-09-10 13:03:10 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: 'template.html',
+    }),
+  ],
+};
