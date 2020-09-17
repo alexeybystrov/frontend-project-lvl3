10e0635e74903de80cedad1d92f61cadffe06089
Alexey Bystrov <alexeybystrov@me.com>
2020-09-17 22:42:17 +0300

    add yup

--- src/index.js
+++ src/index.js
@@ -6,14 +6,65 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 // import 'bootstrap/dist/css/bootstrap-grid.min.css';
 // import 'bootstrap/dist/css/bootstrap.css';
+import onChange from 'on-change';
+import * as yup from 'yup';
+import view from './view';
+// import axios from 'axios';
 
-/* const component = () => {
-  const element = document.createElement('div');
+const schema = yup.string().url();
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+/* const errorMessages = {
+  network: {
+    error: 'Network Problems. Try again.',
+  },
+};
+ */
+const state = {
+  form: {
+    fields: {
+      rssLink: '',
+    },
+    valid: true,
+    validationErrors: [],
+  },
+  feeds: [],
+  posts: [],
 };
 
-document.body.appendChild(component());
+const form = document.querySelector('.rss-form');
+
+/* const watchedState = onChange(state, (path, value) => {
+  switch (path) {
+    case 'form.processState':
+      processStateHandler(value);
+      break;
+    case 'form.valid':
+      submitButton.disabled = !value;
+      break;
+    case 'form.errors':
+      viewErrors(fieldElements, value);
+      break;
+    default:
+      break;
+  }
+});
  */
+
+const watchedState = onChange(state, (/* path, currentValue, previousValue */) => {
+  view(state);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  watchedState.form.fields.rssLink = formData.get('url');
+  try {
+    schema.validateSync(state.form.fields.rssLink, { abortEarly: false });
+    watchedState.form.valid = true;
+    watchedState.form.validationErrors = [];
+  } catch (err) {
+    watchedState.form.valid = false;
+    const errors = err.inner.map(({ path, message }) => ({ [path]: message }));
+    watchedState.form.validationErrors = errors;
+  }
+});
---
+++ src/view.js
@@ -0,0 +1,5 @@
+// import _ from 'lodash';
+
+export default (state) => {
+  console.log(state.form);
+};
